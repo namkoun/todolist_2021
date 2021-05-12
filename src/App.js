1490760d45b746c762b0fc0e1698a516d297bd94
namkoun <70187334+namkoun@users.noreply.github.com>
2021-05-12 12:38:09 +0900

    INIT0512

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import {TextField, Typography} from '@material-ui/core';
+import {KeyboardDatePicker,KeyboardTimePicker} from '@material-ui/pickers'
+import React from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          hi
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      title:"",
+      content:"",
+      startDate: null,
+      startTime: null,
+      endDate:null,
+      endTime:null
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <div className="header">TODO LIST</div>.
+        <div className="input_area">
+          <TextField label="제목" size="normal" margin="normal" fullWidth required />
+          <TextField label="상세내용" size="normal" margin="normal" fullWidth multiline />
+        
+        <KeyboardDatePicker
+         disableToolbar
+         variant="inline"
+         format="yyyy/MM/DD"
+         margin="normal"
+         label="시작 예정일"
+         onChange={(value)=>console.log(value)}
+         style = {{width: '50%'}}
+         KeyboardButtonProps={{'aria-label': 'change date',}}
+         />
+   
+         <KeyboardTimePicker
+         margin="normal"
+         label="시작시간"
+         variant="inline"
+         onChange={(value)=>console.log(value)}
+         style = {{width: '50%'}}
+         KeyboardButtonProps={{'aria-label': 'change time',}}
+         />
+         </div>
+        <div className="list_area">
+          <Typography variant="body2" color="textSecondary" align="center">
+           {'Copyright ㏇ 홍길동 '+new Date().getFullYear()+'.'}
+          </Typography>
+        </div>
+        
+      </div>
+     );
+
+  }
+  
 }
 
 export default App;
