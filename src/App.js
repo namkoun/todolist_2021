2a3b0d72c62f22b920feeab6ece44532fd855f08
namkoun <70187334+namkoun@users.noreply.github.com>
2021-05-26 18:16:05 +0900

    210526_Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import {TextField, Typography} from '@material-ui/core';
+import {Button, TextField, Typography, List, ListItem, ListItemText} from '@material-ui/core';
 import {KeyboardDatePicker,KeyboardTimePicker} from '@material-ui/pickers'
 import React from 'react';
-
+import { Alert } from '@material-ui/lab';
+import SaveIcon from '@material-ui/icons/Save';
 import './App.css';
 
 
@@ -9,22 +10,84 @@ class App extends React.Component{
   constructor(props){
     super(props);
     this.state={
+      todoList:[],
       title:"",
       content:"",
       startDate: null,
       startTime: null,
       endDate:null,
-      endTime:null
+      endTime:null,
+
+    }
+  }
+  checkValidate(){
+    const{
+      title,content,startDate,startTime,endDate,endTime
+    }=this.state;
+    if (!title) {
+      alert("제목을 입력해주세요.");
+      return false;
+    } else if (!content) {
+      alert("내용을 입력해주세요.");
+      return false;
+    } else if (!startDate) {
+      alert("시작일을 입력해주세요.");
+      return false;
+    } else if (!startTime) {
+      alert("시작 시간을 입력해주세요.");
+      return false;
+    } else if (!endDate) {
+      alert("종료일을 입력해주세요.");
+      return false;
+    } else if (!endTime) {
+      alert("종료 시간을 입력해주세요.");
+      return false;
+    } else if(title && content && startDate && startTime && endDate && endTime){
+      return true
     }
   }
 
+  checkValidate1(){
+    const{startDate,endDate}=this.state;
+    if(endDate<startDate){
+      alert("시작일이 더 앞일 수는 없습니다");
+      return false;
+    }
+    return true
+  }
+
+
+
+  saveTodoList(){
+    if(this.checkValidate()){
+      if(this.checkValidate1()){
+    const {todoList,title,content,startDate,startTime,endDate,endTime}= this.state;
+    todoList.push({title: title.trim(), content: content.trim(),startDate,startTime,endDate,endTime});
+    this.setState({
+      todoList,
+      title:"",
+      content:"",
+      startDate:null,
+      startTime:null,
+      endDate:null,
+      endTime:null,
+    });}
+  }
+  }
   render(){
     return (
       <div className="App">
         <div className="header">TODO LIST</div>.
         <div className="input_area">
-          <TextField label="제목" size="normal" margin="normal" fullWidth required />
-          <TextField label="상세내용" size="normal" margin="normal" fullWidth multiline />
+          
+          <TextField label="제목" size="normal" margin="normal" fullWidth required 
+          value={this.state.title}
+          onChange={(e)=> {this.setState({title:e.target.value})}}
+          />
+          <TextField label="상세내용" size="normal" margin="normal" fullWidth multiline
+          value={this.state.content}
+          onChange={(e)=> {this.setState({content:e.target.value})}}
+          />
         
         <KeyboardDatePicker
          disableToolbar
@@ -32,7 +95,8 @@ class App extends React.Component{
          format="yyyy/MM/DD"
          margin="normal"
          label="시작 예정일"
-         onChange={(value)=>console.log(value)}
+         value={this.state.startDate}
+         onChange={(value)=> {this.setState({startDate:value})}}
          style = {{width: '50%'}}
          KeyboardButtonProps={{'aria-label': 'change date',}}
          />
@@ -41,17 +105,68 @@ class App extends React.Component{
          margin="normal"
          label="시작시간"
          variant="inline"
-         onChange={(value)=>console.log(value)}
+         value={this.state.startTime}
+         onChange={(value)=>{this.setState({startTime:value})}}
          style = {{width: '50%'}}
          KeyboardButtonProps={{'aria-label': 'change time',}}
          />
+         <KeyboardDatePicker
+         disableToolbar
+         variant="inline"
+         format="yyyy/MM/DD"
+         margin="normal"
+         label="종료 예정일"
+         value={this.state.endDate}
+         onChange={(value)=> {this.setState({endDate:value})}}
+         style = {{width: '50%'}}
+         KeyboardButtonProps={{'aria-label': 'change date',}}
+         />
+   
+         <KeyboardTimePicker
+         margin="normal"
+         label="종료시간"
+         variant="inline"
+         value={this.state.endTime}
+         onChange={(value)=>{this.setState({endTime:value})}}
+         style = {{width: '50%'}}
+         KeyboardButtonProps={{'aria-label': 'change time',}}
+         />
+        <Button 
+        variant="outlined" 
+        startIcon={<SaveIcon/>} 
+        style={{float:'right'}}
+        onClick={(value)=>this.saveTodoList()}
+        >
+          save
+        </Button>
          </div>
         <div className="list_area">
-          <Typography variant="body2" color="textSecondary" align="center">
-           {'Copyright ㏇ 홍길동 '+new Date().getFullYear()+'.'}
-          </Typography>
-        </div>
+         
+        <List>
+        {this.state.todoList.map((todoItem, idx) => {
+          const{
+            title,content,startDate,startTime,endDate,endTime
+          }=todoItem;
+          return(
+            <ListItem key={idx} role={undefined} dense button>
+              <ListItemText
+              primary={title}
+              secondary={startDate?.format('yyyy-MM-DD'+' '+startTime?.format('HH:MM')+'~'+endDate?.format('yyyy-MM-DD')+' '+endTime?.format('HH:MM'))}
+             
+              />
+                  
+               
+            </ListItem>
+          )
+        })}
         
+
+        </List>
+
+        </div>
+        <Typography variant="body2" color="textSecondary" align="center">
+           {'Copyright ㏇ 김남경 '+new Date().getFullYear()+'.'}
+          </Typography>
       </div>
      );
 
